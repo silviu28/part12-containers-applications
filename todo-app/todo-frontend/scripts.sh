d0ec441051db77f2072ca6de5b4e19036fe7e4e4
silviu <183800984+silviu28@users.noreply.github.com>
2026-02-10 13:07:00 +0200

    refactor: compose and nginx configuration

--- todo-app/todo-frontend/scripts.sh
+++ todo-app/todo-frontend/scripts.sh
@@ -1,2 +1,8 @@
 # start frontend container with published port
 docker run -p 5173:5173 -v "$(pwd):/usr/src/app/" hello-front-dev
+
+# ???
+docker compose  -f docker-compose.dev.yml run debug-helper wget -O - http://app:5173
+
+# run frontend with port mapping
+docker run -p 5001:3000 hello-front
